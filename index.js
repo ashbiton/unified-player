e1882dbfc259cba1121117c4c4d436670c896d57
Aviya Biton <abiton@synamedia.com>
2025-02-24 17:22:40 +0200

    latest changes

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
-import { init, uiReady, lifecycle } from "senza-sdk";
+import { init, uiReady, lifecycle, ShakaPlayer } from "senza-sdk";
 import "./styles.css"
 
 // const TEST_VIDEO = "https://storage.googleapis.com/shaka-demo-assets/angel-one/dash.mpd";
 const TEST_VIDEO = "https://d3aebn8jgh8nvm.cloudfront.net/from-mediaconvert/Sintel-sub/default.mpd";
 
 let player;
-const defaultAudioLang = "por"
-const defaultSubsLang = "fra"
-let audioLangs = [defaultAudioLang];
-let selectedAudioIndex = 0;
-let textLangs = [defaultSubsLang];
-let selectedTextIndex = 0;
+let audioLangs, textLangs, selectedTextIndex, selectedAudioIndex;
 
 window.addEventListener("load", async () => {
   try {
@@ -18,29 +13,25 @@ window.addEventListener("load", async () => {
     player = new ShakaPlayer();
     await player.attach(video);
     player.configure({
-      preferredAudioLanguage: defaultAudioLang,
-      preferredSubtitlesLanguage: defaultSubsLang
+      preferredAudioLanguage: "por",
+      preferredSubtitlesLanguage: "fra"
     })
     player.setTextTrackVisibility(true);
     await player.load(TEST_VIDEO);
     await video.play();
 
-    audioLangs = player.getAudioLanguages();
-    textLangs = player.getTextLanguages();
-    selectedAudioIndex = audioLangs.indexOf(defaultAudioLang);
-    selectedTextIndex = audioLangs.indexOf(defaultSubsLang);
-
-    console.log("audio", audioLangs, selectedAudioIndex);
-    console.log("text", textLangs, selectedTextIndex);
-
-    player.remotePlayer.addEventListener("tracksupdate", () => {
-      console.log("Loaded tracks!")
-      console.log("remote audio", player.remotePlayer.getAudioTracks());
-      console.log("remote text", player.remotePlayer.getTextTracks());
-      player.selectAudioLanguage(audioLangs[selectedAudioIndex]);
-      player.selectTextLanguage(textLangs[selectedTextIndex]);
-    });
-
+    setTimeout(() => {
+      audioLangs = player.getAudioLanguages();
+      textLangs = player.getTextLanguages();
+      /**
+       * this is a hack. we are relaying on the fact that we know (!) the languages in the MPD. 
+       * what we need to do is use the getVariants to find the selected tracks.
+       */
+      selectedAudioIndex = audioLangs.indexOf("pt");
+      selectedTextIndex = textLangs.indexOf("fr");
+      console.log("INFO", audioLangs, textLangs, selectedAudioIndex, selectedTextIndex);
+      updateBanner()
+    }, 3000)
     uiReady();
   } catch (error) {
     console.error(error);
@@ -64,26 +55,34 @@ async function toggleBackground() {
   if (lifecycle.state == lifecycle.UiState.BACKGROUND) {
     await lifecycle.moveToForeground();
   } else {
-    console.log("Setting audio langauge before moving to background");
-    player.selectAudioLanguage(audioLangs[selectedAudioIndex]);
+    // console.log("Setting audio language before moving to background");
+    // player.selectAudioLanguage(audioLangs[selectedAudioIndex]);
     await lifecycle.moveToBackground();
   }
 }
 
 function changeAudioLang(delta) {
+  if (selectedAudioIndex === undefined || audioLangs === undefined) {
+    console.warn("cannot change audio lang. ", selectedAudioIndex, audioLangs);
+    return
+  }
   selectedAudioIndex = (selectedAudioIndex + delta + audioLangs.length) % audioLangs.length;
   player.selectAudioLanguage(audioLangs[selectedAudioIndex]);
   updateBanner();
 }
 
 function changeTextLang(delta) {
+  if (selectedTextIndex === undefined || textLangs === undefined) {
+    console.warn("cannot change text lang. ", selectedTextIndex, textLangs);
+    return
+  }
   selectedTextIndex = (selectedTextIndex + delta + textLangs.length) % textLangs.length;
   player.selectTextLanguage(textLangs[selectedTextIndex]);
   updateBanner();
 }
 
 function updateBanner() {
-  banner.innerHTML =
+  document.getElementById("banner").innerHTML =
     `audio: ${audioLangs[selectedAudioIndex]}<br>` +
     `text: ${textLangs[selectedTextIndex]}`
 }
